6f757a71aca8d42d0458cdb81b7a6c13976df8a2
Jay <thanawat352@gmail.com>
2023-12-03 19:16:48 +0700

    main.go ยังติด error อยู่

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,9 @@
- 
-
 package main
 
-
 import (
-
-"github.com/gin-gonic/gin"
-"github.com/JayPornananratKan/TE-10/entity"	
-"github.com/JayPornananratKan/TE-10/controller"
-
-
-
+	"github.com/JayPornananratKan/TE-10/controller"
+	"github.com/JayPornananratKan/TE-10/entity"
+	"github.com/gin-gonic/gin"
 )
 
 
@@ -23,16 +16,19 @@ r := gin.Default()
 r.Use(CORSMiddleware())
 
 // User Routes
+r.GET("/payments", controller.ListPayment)
+r.GET("/payments/:id", controller.GetPayment)
+r.POST("/payments", controller.CreatePayment)
+r.DELETE("/payments", controller.DeletePayment)
+r.PATCH("/payments/:id",controller.UpdatePayment)
 
-r.GET("/users", controller.ListUsers)
+r.POST("/repairrequests".controller.CreateRepairrequest)
+r.GET("/repairrequests".controller.ListRepairrequest)
+r.GET("/repairrequests/:id".controller.GetRepairrequest)
 
-r.GET("/user/:id", controller.GetUser)
+r.GET("/typemembers".controller.ListTypemember)
+r.GET("/typemembers/:id".controller.GetTypemember)
 
-r.POST("/users", controller.CreateUser)
-
-r.PATCH("/users", controller.UpdateUser)
-
-r.DELETE("/users/:id", controller.DeleteUser)
 
 // Run the server
 
