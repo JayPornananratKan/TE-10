a0a3e6e100655a4823ebce9d4c245f2c5069256e
Jay <thanawat352@gmail.com>
2023-12-04 18:15:12 +0700

    main เสร็จ

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/JayPornananratKan/TE-10/controller"
+	
 	"github.com/JayPornananratKan/TE-10/entity"
 	"github.com/gin-gonic/gin"
 )
@@ -16,19 +17,21 @@ r := gin.Default()
 r.Use(CORSMiddleware())
 
 // User Routes
+r.POST("/payments", controller.CreatePayment)
 r.GET("/payments", controller.ListPayment)
 r.GET("/payments/:id", controller.GetPayment)
-r.POST("/payments", controller.CreatePayment)
-r.DELETE("/payments", controller.DeletePayment)
-r.PATCH("/payments/:id",controller.UpdatePayment)
 
-r.POST("/repairrequests".controller.CreateRepairrequest)
-r.GET("/repairrequests".controller.ListRepairrequest)
-r.GET("/repairrequests/:id".controller.GetRepairrequest)
+r.DELETE("/payments/:id", controller.DeletePayment)
+r.PATCH("/payments",controller.UpdatePayment)
 
-r.GET("/typemembers".controller.ListTypemember)
-r.GET("/typemembers/:id".controller.GetTypemember)
+r.GET("/typemembers", controller.ListTypemember)
+r.GET("/typemembers/:id", controller.GetTypemember)
 
+r.POST("/repairrequests" , controller.CreateRepairrequest)
+r.GET("/repairrequests", controller.ListRepairrequest)
+r.GET("/repairrequests/:id", controller.GetRepairrequest)
+r.DELETE("/repairrequests/:id", controller.DeleteRepairrequest)
+r.PATCH("/repairrequests", controller.UpdateRepairrequest)
 
 // Run the server
 
